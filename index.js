dabbcc931b8fe65f9c6de5155b0ebff66c250df4
Sam David <sfd1987@gmail.com>
2019-04-29 01:02:50 -0700

    inital users routes and pg setup

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3001
+const db = require('./queries')
 
 app.use(bodyParser.json())
 app.use(
@@ -14,7 +15,16 @@ app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
 })
 
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
 
+
+// Start listen (leave last)
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
 })
+
+
